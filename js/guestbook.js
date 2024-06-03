04887edcef5c1ed983d655ba91428dd1b6d6ccc4
jaeboklee <taland7@naver.com>
2024-06-03 20:14:35 +0900

    hotfix

--- js/guestbook.js
+++ js/guestbook.js
@@ -3,33 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const guestbookEntries = document.getElementById("guestbook-entries");
 
   async function fetchEntries() {
-    try {
-      const response = await axios.get("http://35.173.37.0:8000/api/entries");
-      const entries = response.data.entries;
+    const response = await axios.get("http://35.173.37.0:8000");
+    const entries = response.data.entries;
+    console.log(entries);
 
-      console.log(entries);
-
-      guestbookEntries.innerHTML = "";
-      if (Array.isArray(entries)) {
-        entries.forEach((entry) => {
-          const entryElement = document.createElement("li");
-          entryElement.classList.add("entry");
-          entryElement.innerHTML = `
-            <header>
-              <strong>${entry.name}</strong>
-              <time>${new Date(entry.timestamp).toLocaleString()}</time>
-            </header>
-            <p>${entry.message}</p>
-            <button data-id="${entry.id}">Delete</button>
-          `;
-          guestbookEntries.appendChild(entryElement);
-        });
-      } else {
-        console.error("Fetched data is not an array:", entries);
-      }
-    } catch (error) {
-      console.error("There was an error fetching entries!", error);
-    }
+    guestbookEntries.innerHTML = "";
+    entries.forEach((entry) => {
+      const entryElement = document.createElement("li");
+      entryElement.classList.add("entry");
+      entryElement.innerHTML = `
+                <header>
+                    <strong>${entry.name}</strong>
+                    <time>${new Date(entry.timestamp).toLocaleString()}</time>
+                </header>
+                <p>${entry.message}</p>
+                <button data-id="${entry.id}">Delete</button>
+            `;
+      guestbookEntries.appendChild(entryElement);
+    });
   }
 
   form.addEventListener("submit", async function (e) {
@@ -37,34 +28,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const name = document.getElementById("author").value;
     const message = document.getElementById("content").value;
 
-    try {
-      const response = await axios.post("http://35.173.37.0:8000/api/entries", {
-        name,
-        message,
-      });
+    const response = await axios.post("http://35.173.37.0:8000", {
+      name,
+      message,
+    });
 
-      if (response.status === 200) {
-        fetchEntries();
-        form.reset();
-      }
-    } catch (error) {
-      console.error("There was an error submitting the entry!", error);
+    if (response.status === 200) {
+      fetchEntries();
+      form.reset();
     }
   });
 
   guestbookEntries.addEventListener("click", async function (e) {
     if (e.target.tagName === "BUTTON") {
       const entryId = e.target.getAttribute("data-id");
-      try {
-        const response = await axios.delete(
-          `http://35.173.37.0:8000/api/entries/${entryId}`
-        );
+      const response = await axios.delete(`http://35.173.37.0:8000${entryId}`);
 
-        if (response.status === 200) {
-          fetchEntries();
-        }
-      } catch (error) {
-        console.error("There was an error deleting the entry!", error);
+      if (response.status === 200) {
+        fetchEntries();
       }
     }
   });
